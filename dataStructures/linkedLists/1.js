26e13d0e060649c32159e74d4549266f5e513d16
unknown <m.edun@providus.mu>
2020-09-03 09:07:26 +0400

    added data structures

---
+++ dataStructures/linkedLists/1.js
@@ -0,0 +1,40 @@
+
+class Node {
+    constructor(value, next){
+        this.value = value;
+        this.next = next;
+    }
+}
+
+
+class SinglyLinkedList {
+    
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push = (val) => {
+
+        const newNode = new Node(val, null);
+        if (!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+            
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length = this.length + 1;
+            return this;
+    }
+}
+
+
+const list = new SinglyLinkedList();
+console.log(list.push(1));
+console.log(list.push(10));
+console.log(list.push(100));
+console.log(list.push(1000));
+
